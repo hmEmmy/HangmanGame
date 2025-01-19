4de3c96695d379f68e1c2491b62682ec7c228f90
Emmy <hmemmy209@gmail.com>
2025-01-20 00:06:15 +0100

    Initial commit

---
+++ src/main/java/me/emmy/project/Main.java
@@ -0,0 +1,11 @@
+package me.emmy.project;
+
+/**
+ * @author Emmy
+ * @project Default (Template) Project
+ * @date 19/01/2025 - 23:55
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
